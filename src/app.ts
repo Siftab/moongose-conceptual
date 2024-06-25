cfb179ac5d2c7868a7546b3e478e798ab20c8604
Siftab <siiffuuu@gmail.com>
2024-06-26 01:17:40 +0600

    basic setup

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
 import express, { Request, Response } from 'express'
 export const app = express()
-require('dotenv').config();
+// require('dotenv').config();
 const port = 5000
 
 app.get('/', (req: Request, res: Response) => {
--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,19 @@
 import mongoose from "mongoose";
 import { app } from "./app";
+import config from "./config";
 
 const port = 5000
 
 async function main() {
     try {
-        await mongoose.connect(`${process.env.DB_URL}`);
+        await mongoose.connect(`${config.db_url}`);
 
         // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
         // appolo-flix
         // 2J6NF13Ul94pex7f
 
-        app.listen(process.env.PORT, () => {
-            console.log(`server is  listening on port ${process.env.PORT}`)
+        app.listen(config.port, () => {
+            console.log(`server is  listening on port ${config.port}`)
         })
 
     } catch (error) {
