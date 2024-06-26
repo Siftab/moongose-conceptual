1fcc5b50e7652c25ecbc6721184dd0ef27ac83d9
Siftab <siiffuuu@gmail.com>
2024-06-26 10:54:28 +0600

    configuring eslint files

--- src/server.ts
+++ src/server.ts
@@ -6,7 +6,7 @@ const port = 5000
 
 async function main() {
     try {
-        await mongoose.connect(`${config.db_url}`);
+        await mongoose.connect(config.db_url as string);
 
         // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
         // appolo-flix
